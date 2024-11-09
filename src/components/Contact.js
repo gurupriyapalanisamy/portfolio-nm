b5b1bb5379a4214584e255267d19808ec3f14abc
gurupriyapalanisamy <146083288+gurupriyapalanisamy@users.noreply.github.com>
2024-11-09 14:38:35 +0530

    Update Contact.js

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Contact = () => (
   <section id="contact">
     <h2>Contact</h2>
-    <p><b>Email:</b> santhoshgpcet@gmail.com</p>
+    <p><b>Email:</b> gurupriyapgpcet@gmail.com</p>
     <p><b>LinkedIn:</b> https://www.linkedin.com/in/santhosh-murugan-14a8a7302
     </p>
     <a className='top' href='#head'>Back to top</a>
